eb26dfa1f92c9d2dc170c3f965729d7a194b9abb
Carlos Mazon <57258681+reithal@users.noreply.github.com>
2020-02-03 23:07:55 -0500

    Updated About page, fixed JavaScript error

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,8 @@
 var currentDate = moment();
 $('#currentDate').html(currentDate.format("dddd MMMM Do, YYYY"));
 var todayDay = currentDate.format("M/DD/YYYY");
-
 var workday = {
-  dateSet: todayDay,
+  dateSet: "",
   events : [
     {
       hour : "9:00 AM",
@@ -50,36 +49,65 @@ var workday = {
       hour : "5:00 PM",
       description : ""
     }
-    ],
-    constructDay: function () {
-      $('tbody').empty();
-      for (i = 0; i < this.events.length; i++) {
-        var hourRow = $('<tr>');
-        hourRow.append('<td style="width: 10%; text-align: center" class="col">' + this.events[i].hour + '</td>');
-        hourRow.append('<td class="col"><input id="inputIndex' + i + '" type="text" placeholder="" value="' + this.events[i].description + '"></td>');
-        hourRow.append('<td class="col"><button class="save-btn" data-index="' + i + '" type="submit"><i class="far fa-save"></i></button></td>');
-        $('tbody').append(hourRow);
-      }
-    },
-    setEventDesc: function (index, eventDesc) {
-      console.log("show arguments " + index +"," + eventDesc );
-      console.log(this.events[index]);
-      
-      this.events[index].description = eventDesc;
-      console.log(eventDesc + " being passed to set and index " + index + " to event" + this.events[index])
-      this.constructDay();
-      console.log(this)
+    ]
+
+    
+  }
+  function constructDay () {
+    $('tbody').empty();
+
+    for (i = 0; i < workday.events.length; i++) {
+      var hourRow = $('<tr>');
+      hourRow.append('<td style="width: 10%; text-align: center" class="col">' + workday.events[i].hour + '</td>');
+      hourRow.append('<td class="col"><input id="inputIndex' + i + '" type="text" placeholder="" value="' + workday.events[i].description + '"></td>');
+      hourRow.append('<td class="col"><button class="save-btn" data-index="' + i + '" type="submit"><i class="far fa-save"></i></button></td>');
+      $('tbody').append(hourRow);
     }
   }
- 
-  workday.constructDay();
 
+
+  function setEventDesc (index, eventDesc) {     
+    workday.events[index].description = eventDesc;
+    // DEBUG: console.log(eventDesc + " being passed to set and index " + index + " to event: " + workday.events[index].description + " for hour " + workday.events[index].hour)
+    constructDay();
+  }
+
+  function checkLocal() {
+    var workingDay = JSON.parse(localStorage.getItem("DATE-" + todayDay));
+    if (workingDay === null){
+      console.log("true for checkLocal")
+      return null;   
+    } 
+    else {
+      console.log("false for checklocal")
+      return workingDay;
+    }
+  }
+
+  function writeLocal () {
+    localStorage.setItem("DATE-" + todayDay , JSON.stringify(workday));
+  };
+
+
+  var data = checkLocal();
+  if (data === null) {
+    workday.dateSet = todayDay;
+    constructDay();
+  }
+  else {
+    workday = data;
+    console.log(workday);
+    constructDay();
+   
+  }
+  
   $(document).on("click", ".save-btn", function(){
     var indexToSave = $(this).attr("data-index");
     var inputDesc = ($("#inputIndex"+indexToSave).val());
-    workday.setEventDesc(indexToSave, inputDesc);
-    localStorage.setItem("DATE-Event", JSON.stringify(workday));
-    workday.constructDay();
-    
-  })
-
+    setEventDesc(indexToSave, inputDesc);
+    writeLocal();
+  });
+  
+  $(document).on("click", ".next-btn", function(){
+    currentDate.add(1, "day")
+  });
